68b049cf6b85d66002878505410229f1f534a050
Adarsh <adarsh.yadav@relesol.com>
2021-05-11 10:20:00 +0530

    checkout

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var cors = require('cors')
 
 var app = express();
+
+app.use(cors())
+ 
 
 app.use(bodyparser.json({
     limit: "50mb"
